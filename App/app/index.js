aa4744f66be987a60904ed112bd6d73ad1ade54c
Casper Søgård <casper-s@hotmail.dk>
2024-03-11 10:15:16 +0100

    Setting up routing for app

---
+++ App/app/index.js
@@ -0,0 +1,22 @@
+import { View, Text, StyleSheet } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+
+function Homepage() {
+    return (
+        <View style={styles.container}>
+        <Text>Test App!</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+});
+
+export default Homepage;
