327f8d5b8b3173f3806899cbe65738217901f328
Jaffenheimer <jfog20@student.aau.dk>
2024-03-22 13:56:06 +0100

    Added bus table

---
+++ backend/p8mobility.dbup/scripts/Bus.sql
@@ -0,0 +1,11 @@
+START TRANSACTION;
+
+CREATE TABLE IF NOT EXISTS Bus
+(
+    Id        VARCHAR(40) PRIMARY KEY,
+    Latitude  DECIMAL(10, 8),
+    Longitude DECIMAL(11, 8),
+    Action    ENUM ('Default', 'Accelerate', 'Decelerate','MaintainSpeed') DEFAULT 'Default'
+);
+
+COMMIT;
