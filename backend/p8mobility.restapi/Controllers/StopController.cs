3a25a17d26671089b36c55947bdd3cf55f5bc0d4
JJTM20 <jjtm20@student.aau.dk>
2024-03-25 10:02:31 +0100

    b4 merging people_detection

---
+++ backend/p8mobility.restapi/Controllers/StopController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace p8_restapi.Controllers;
+
+[ApiController]
+[Route("stops")]
+public class StopController : ControllerBase
+{
+    public StopController()
+    {
+    }
+
+    [HttpPost("GetPeopleCount")]
+    public async Task<IActionResult> FetchPeopleCount()
+    {
+        return Ok("Okidoki");
+    }
+}
