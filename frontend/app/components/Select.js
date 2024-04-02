19f81705e16c92b3f280636338a8217554d596f6
Nikolaj Kofod Krogh <74181362+NikolajKrogh@users.noreply.github.com>
2024-04-02 09:34:49 +0200

    Added some style for ios picker

--- frontend/app/components/Select.js
+++ frontend/app/components/Select.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import RNPickerSelect from 'react-native-picker-select';
 import { Container } from '~/tamagui.config';
 import { Stack } from 'expo-router';
+import { View, Text } from 'tamagui';
 
 const Select = ({ items, title, onChange }) => {
     const [value, setValue] = useState('');
@@ -14,23 +15,35 @@ const Select = ({ items, title, onChange }) => {
     return (
         <Container>
             <Stack.Screen />
-            <RNPickerSelect
-                onValueChange={onChangeFunction}
-                items={items.map((item) => ({ label: item, value: item }))}
-                touchableWrapperProps={{ testID: 'picker-select' }}
-                style={{
-                    inputIOS: {
-                        backgroundColor: '#6366F1',
-                        color: 'white',
-                    },
-                    inputAndroid: {
-                        backgroundColor: '#6366F1',
-                        color: 'white',
-                    },
-                }}
-                placeholder={{ label: `--- ${title} ---`, value: null }}
-                value={value}
-            />
+            <View>
+                <Text>{title}</Text>
+                <RNPickerSelect
+                    onValueChange={onChangeFunction}
+                    items={items.map((item) => ({ label: item, value: item }))}
+                    touchableWrapperProps={{ testID: 'picker-select' }}
+                    style={{
+                        inputIOS: {
+                            fontSize: 16,
+                            paddingVertical: 12,
+                            paddingHorizontal: 10,
+                            borderWidth: 1,
+                            borderColor: 'gray',
+                            borderRadius: 4,
+                            color: 'black',
+                            paddingRight: 30, // to ensure the text is never behind the icon
+                            backgroundColor: '#6366F1',
+                            borderRadius: 10,
+                            height: 50,
+                        },
+                        inputAndroid: {
+                            backgroundColor: '#6366F1',
+                            color: 'white',
+                        },
+                    }}
+                    placeholder={{ label: `--- ${title} ---`, value: null }}
+                    value={value}
+                />
+            </View>
         </Container>
     );
 };
