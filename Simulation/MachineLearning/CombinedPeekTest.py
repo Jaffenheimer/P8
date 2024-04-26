44c208322e9e28db7d518ef0db02f7dcece904fd
SebxGit <72699667+sebxGit@users.noreply.github.com>
2024-04-26 13:51:34 +0200

    addded ModelPeek

--- Simulation/MachineLearning/CombinedPeekTest.py
+++ Simulation/MachineLearning/CombinedPeekTest.py
@@ -1,36 +1,38 @@
-from PPOPeekVersion import PPOPeekVersion
-from A2CPeekVersion import A2CPeekVersion
+from stable_baselines3 import PPO, A2C
+from sb3_contrib import RecurrentPPO, TRPO
 from Helper.PlotDiagram import PlotAveragePeopleAtBusstopsMultiple, PlotAverageWaitTimeMultiple
-from TRPOPeekVersion import TRPOPeekVersion
 from RandomVersion import RandomVersion
 from GreedyFastVersion import GreedyFastVersion
 from ScheduleVersion import ScheduleVersion
+import time
+import ModelPeek
 
-def CombinedPeekTest():
+def CombinedTest():
 
-    data_PPO = PPOPeekVersion()
-    print("PPO done")
+    start_time = time.time()
 
-    data_A2C = A2CPeekVersion()
-    print("A2C done")
+    data_PPO = ModelPeek.run(PPO, "PPO", "MlpPolicy")
+    
+    data_recurrent_PPO = ModelPeek.run(RecurrentPPO, "Recurrent PPO", "MlpLstmPolicy")
 
-    data_TRPO = TRPOPeekVersion()
-    print("TRPO done")
+    data_A2C = ModelPeek.run(A2C, "A2C", "MlpPolicy")
+
+    data_TRPO = ModelPeek.run(TRPO, "TRPO", "MlpPolicy")
 
     data_random = RandomVersion()
-    print("Random done")
 
     data_schedule = ScheduleVersion()
-    print("Schedule done")
 
     data_greedy = GreedyFastVersion()
-    print("Greedy Fast done")
-    
-    PlotAverageWaitTimeMultiple(("Random",data_random),("Greedy",data_greedy), ("PPO",data_PPO),
-                                ("TRPO",data_TRPO), ("A2C",data_A2C), ("Schedule",data_schedule))
 
-    PlotAveragePeopleAtBusstopsMultiple(("Random",data_random),("Greedy",data_greedy), ("PPO",data_PPO),
-                                ("TRPO",data_TRPO), ("A2C",data_A2C), ("Schedule",data_schedule))
+    print(f"Total time: {time.time() - start_time:.2f} seconds")
+
+    PlotAverageWaitTimeMultiple(("Random", data_random), ("Greedy", data_greedy), ("PPO", data_PPO), ("Recurrent PPO", data_recurrent_PPO),
+                                ("TRPO", data_TRPO), ("A2C", data_A2C), ("Schedule", data_schedule))
+
+    PlotAveragePeopleAtBusstopsMultiple(("Random", data_random), ("Greedy", data_greedy), ("PPO", data_PPO), ("Recurrent PPO", data_recurrent_PPO),
+                                        ("TRPO", data_TRPO), ("A2C", data_A2C), ("Schedule", data_schedule))
+
 
 if __name__ == "__main__":
-    CombinedPeekTest()
+    CombinedTest()
---
+++ Simulation/MachineLearning/ModelPeek.py
@@ -0,0 +1,69 @@
+from random import randint
+import numpy as np
+from stable_baselines3 import A2C, PPO
+from sb3_contrib import TRPO, RecurrentPPO
+from stable_baselines3.common.env_util import make_vec_env
+from SumoEnvironment import SumoEnv
+from Constants import TOTAL_TIMESTEPS, MAX_STEPS, N_ENVS, PEEK_INTERVAL, PEEK_LEARN_STEPS
+from Helper.PlotDiagram import PlotBoth
+
+def run(modelType,name,policy):
+    print(f"====================== <{name} Init> ======================")
+
+    # Importing the environment
+    env = make_vec_env(SumoEnv, n_envs=N_ENVS)
+
+    #alternatively we could add such that you can pass the arguments to this function directly into the run function (as a dictionary) like this
+    model_params = {"policy": policy, "env": env, "verbose": 1, "n_steps": MAX_STEPS}
+    if modelType != A2C:
+        model_params["batch_size"] = 80
+    
+    # Create the agent
+    model = modelType(**model_params)
+
+    print(f"====================== <{name} Training> ======================")
+
+    # Train the agent
+    # model.learn(total_timesteps=TOTAL_TIMESTEPS, progress_bar=True)
+
+    # Save the agent
+    # model.save(f"{name}")
+
+    # del model  # remove to demonstrate saving and loading
+
+    # Load the trained agent
+    # model = modelType.load(f"{name}")
+
+    print(f"====================== <{name} Traning Completed> ======================")
+
+    # Test the agent
+    obs = env.reset()
+    dtype = [('AveragePeopleAtBusStops', float), ('AverageWaitTime', float)]
+    data = np.zeros(MAX_STEPS, dtype=dtype)
+    step = 0
+    done = np.array([False], dtype='bool')
+
+    for _ in range(MAX_STEPS):
+        action, _ = model.predict(obs)
+
+        obs, rewards, done, info = env.step(action)
+        np.set_printoptions(suppress=True, precision=3, floatmode="fixed")
+        data['AverageWaitTime'][step] = obs.item(0)
+        data['AveragePeopleAtBusStops'][step] = obs.item(1)
+        step += 1
+
+        if done.all():
+            env.close()
+            break
+        elif step % PEEK_INTERVAL == PEEK_INTERVAL:
+            model.learn(PEEK_LEARN_STEPS)
+
+    print(f"====================== <{name} Done> ======================")
+    return data[:-1]
+
+if __name__ == "__main__":
+    data = run(PPO, "PPO", "MlpPolicy")
+    data = run(RecurrentPPO, "Recurrent PPO", "MlpLstmPolicy")
+    data = run(A2C, "A2C", "MlpPolicy")
+    data = run(TRPO, "TRPO", "MlpPolicy")
+    PlotBoth(data)
