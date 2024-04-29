8ba39989d2918284293d021f3e4bd2f12b6169e4
Casper0913 <casper-s@hotmail.dk>
2024-04-29 08:20:48 +0200

    Fixes to tuning

--- Simulation/MachineLearning/CombinedTuning.py
+++ Simulation/MachineLearning/CombinedTuning.py
@@ -5,37 +5,20 @@ from stable_baselines3.common.env_util import make_vec_env
 from sb3_contrib import TRPO, RecurrentPPO
 from optuna.samplers import TPESampler
 import numpy as np
-from Constants import TUNING_STEPS, TUNING_TOTAL_TIMESTEPS, TUNING_TRIALS
+from Helper.ModelParams import get_model_params
+from Constants import TUNING_TOTAL_TIMESTEPS, TUNING_TRIALS, TUNING_STEPS
 from SumoEnvironment import SumoEnv
 
-''' Functions for combined tunning of hyperparameters of different model (PPO, A2C, TRPO, etc.)'''
 
+def Objective(trial: optuna.Trial, modelType, policy):  
+    
+    model_params, n_envs = get_model_params(modelType, trial)
+    
+    model_params["policy"] = policy
 
-def Objective(trial: optuna.Trial, modelType, policy):
-    # Define hyperameters to tune
-
-    n_envs = trial.suggest_int("n_envs", 4, 8)
-    learning_rate = trial.suggest_float("learning_rate", 1e-5, 1e-2, log=True)
-    gamma = trial.suggest_float("gamma", 0.9, 0.999, log=True)
-    gae_lambda = trial.suggest_float("gae_lambda", 0.8, 0.99, log=True)
-
-    if modelType != A2C or modelType != TRPO:
-        batch_size = trial.suggest_int("batch_size", 64, 128, log=True)
-        ent_coef = trial.suggest_float("ent_coef", 0.00000001, 0.1, log=True)
-        vf_coef = trial.suggest_float("vf_coef", 0.00000001, 0.5, log=True)
-
-        # Importing the environment
     env = make_vec_env(SumoEnv, n_envs=n_envs)
+    model_params["env"] = env
 
-    model_params = {"policy": policy, "env": env, "verbose": 0, "learning_rate": learning_rate,
-                    "gamma": gamma, "gae_lambda": gae_lambda, "device": 'auto', "n_steps": TUNING_STEPS}
-
-    if modelType != A2C or modelType != TRPO:
-        model_params["ent_coef"] = ent_coef
-        model_params["vf_coef"] = vf_coef
-        model_params["batch_size"] = batch_size
-
-    # Create the model with the hyperparameters
     model = modelType(**model_params)
 
     # Train the model
@@ -60,19 +43,21 @@ def Objective(trial: optuna.Trial, modelType, policy):
 def CombinedTuning(modelType, policy, name):
     study = optuna.create_study(direction="minimize", sampler=TPESampler())
     study.optimize(lambda trial: Objective(
-        trial, modelType, policy), n_trials=TUNING_TRIALS)
+        trial, modelType, policy), n_trials=TUNING_TRIALS, show_progress_bar=True)
 
     best_trial = study.best_trial.params
-    print(f"Best trial: {best_trial}")
 
-    if not path.isdir("./Simulation/MachineLearning/Output"):
-        mkdir("./Simulation/MachineLearning/Output")
-    with open(f"./Simulation/MachineLearning/Output/best_trial_{name}.csv", "w") as f:
-        f.write(str(best_trial))
+    if not path.isdir("./Simulation/MachineLearning/Output/TuningFiles"):
+        mkdir("./Simulation/MachineLearning/Output/TuningFiles")
+    with open(f"./Simulation/MachineLearning/Output/TuningFiles/best_trial_{name}.csv", "w") as f:
+        f.write(
+            f"Best trial for {name} (Trials: {TUNING_TRIALS}, Steps: {TUNING_STEPS}, Total Timesteps: {TUNING_TOTAL_TIMESTEPS}):\n")
+        for key, value in best_trial.items():
+            f.write(f"{key}: {value}\n")
 
 
 if __name__ == "__main__":
-    CombinedTuning(PPO, "MlpPolicy", "PPO")
-    # CombinedTuning(A2C, "MlpPolicy", "A2C")
+    # CombinedTuning(PPO, "MlpPolicy", "PPO")
+    CombinedTuning(A2C, "MlpPolicy", "A2C")
     # CombinedTuning(TRPO, "MlpPolicy", "TRPO")
     # CombinedTuning(RecurrentPPO, "MlpLstmPolicy", "RecurrentPPO")
---
+++ Simulation/MachineLearning/Helper/ModelParams.py
@@ -0,0 +1,73 @@
+from stable_baselines3 import A2C, PPO
+from sb3_contrib import TRPO, RecurrentPPO
+from Constants import TUNING_STEPS
+
+
+''' Functions for combined tunning of hyperparameters of different model (PPO, A2C, TRPO, etc.)'''
+def get_model_params(modelType, trial):
+
+    n_envs = trial.suggest_int("n_envs", 4, 8)  # Number of environments
+
+    base_params = {
+        "policy": None,
+        "env": None,
+        "verbose": 0,
+        "gamma": trial.suggest_float("gamma", 0.9, 0.999, log=True),
+        "gae_lambda": trial.suggest_float("gae_lambda", 0.8, 0.99, log=True),
+        "learning_rate": trial.suggest_float("learning_rate", 1e-5, 1e-2, log=True),
+        "device": 'auto',
+        "n_steps": TUNING_STEPS,
+        "gamma": trial.suggest_float("gamma", 0.9, 0.999, log=True),
+        "gae_lambda": trial.suggest_float("gae_lambda", 0.8, 0.99, log=True),
+    }
+
+    if modelType == A2C:
+        a2c_params = {
+            "learning_rate": trial.suggest_float("learning_rate", 1e-5, 1e-2, log=True),
+            # "gamma": trial.suggest_float("gamma", 0.9, 0.999, log=True),
+            # "gae_lambda": trial.suggest_float("gae_lambda", 0.8, 0.99, log=True),
+            "ent_coef": trial.suggest_float("ent_coef", 0.00000001, 0.1, log=True),
+            "vf_coef": trial.suggest_float("vf_coef", 0.00000001, 0.5, log=True),
+            "max_grad_norm": trial.suggest_float("max_grad_norm", 0.3, 1.0, log=True)
+        }
+        base_params.update(a2c_params)
+    elif modelType == PPO:
+        ppo_params = {
+            "learning_rate": trial.suggest_float("learning_rate", 1e-5, 1e-2, log=True),
+            "batch_size": trial.suggest_int("batch_size", 64, 128, log=True),
+            "clip_range": trial.suggest_float("clip_range", 0.1, 0.4),
+            "ent_coef": trial.suggest_float("ent_coef", 0.00000001, 0.1, log=True),
+            "vf_coef": trial.suggest_float("vf_coef", 0.00000001, 0.5, log=True),
+            "max_grad_norm": trial.suggest_float("max_grad_norm", 0.3, 1.0, log=True),
+            # "gamma": trial.suggest_float("gamma", 0.9, 0.999, log=True),
+            # "gae_lambda": trial.suggest_float("gae_lambda", 0.8, 0.99, log=True)
+        }
+        base_params.update(ppo_params)
+    elif modelType == TRPO:
+        trpo_params = {
+            "learning_rate": trial.suggest_float("learning_rate", 1e-5, 1e-2, log=True),
+            "batch_size": trial.suggest_int("batch_size", 64, 128, log=True),
+            # "gamma": trial.suggest_float("gamma", 0.9, 0.999, log=True),
+            # "gae_lambda": trial.suggest_float("gae_lambda", 0.8, 0.99, log=True),
+            "target_kl": trial.suggest_float("target_kl", 0.01, 0.1),
+            "cg_max_steps": trial.suggest_int("cg_max_steps", 10, 20),
+            "cg_damping": trial.suggest_float("cg_damping", 0.1, 0.3),
+            "line_search_shrinking_factor": trial.suggest_float("line_search_shrinking_factor", 0.1, 0.3),
+            "line_search_max_iter": trial.suggest_int("line_search_max_iter", 10, 20),
+            "n_critic_updates": trial.suggest_int("n_criitc_updates", 1, 10)
+        }
+        base_params.update(trpo_params)
+    elif modelType == RecurrentPPO:
+        recurrent_ppo_params = {
+            "learning_rate": trial.suggest_float("learning_rate", 1e-5, 1e-2, log=True),
+            "batch_size": trial.suggest_int("batch_size", 64, 128, log=True),
+            "clip_range": trial.suggest_float("clip_range", 0.1, 0.4),
+            "gamma": trial.suggest_float("gamma", 0.9, 0.999, log=True),
+            "gae_lambda": trial.suggest_float("gae_lambda", 0.8, 0.99, log=True),
+            "ent_coef": trial.suggest_float("ent_coef", 0.00000001, 0.1, log=True),
+            "vf_coef": trial.suggest_float("vf_coef", 0.00000001, 0.5, log=True),
+            "max_grad_norm": trial.suggest_float("max_grad_norm", 0.3, 1.0, log=True)
+        }
+        base_params.update(recurrent_ppo_params)
+
+    return base_params, n_envs
